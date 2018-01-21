17df4b402c17dfee7a4a0da43f64fa36e339a838
David Iliaguiev <iliaguievdavid@gmail.com>
2018-01-21 14:35:49 -0500

    add React

--- server/index.js
+++ server/index.js
@@ -1,19 +1,26 @@
-const http = require('http');
-
 // event emitters - need to learn
 // streams
 // clusters
 
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+
+/*
 const server = http.createServer();
 
 server.on('request', (req, res)=> {
-  res.write("What up pussy!?!\n");
-
-  setTimeout(() => {
-    res.write("Still on. ...");
-    res.end();
-  }, 3000)
-  
+  res.end(fs.readFileSync(__dirname + '/../client/index.html'));
 });
 
 server.listen(8080);
+*/
+
+const app = express();
+
+app.use(express.static('client'));
+//app.get('/', (req, res) => {
+//  res.sendFile(__dirname + '../client/index.html');
+//});
+
+app.listen(8080);

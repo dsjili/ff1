137eb0053211ad4bc828e2b6bcc7d33474dee358
David Iliaguiev <iliaguievdavid@gmail.com>
2018-02-10 12:59:57 -0500

    class working

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,17 @@
-const App = () => {
-	return (
-		<h2>Hello components</h2>
-	);
-};
+// const App = () => {
+// 	return (
+// 		<h2>Hello components</h2>
+// 	);
+// };
+
+class App extends React.Component {
+	state = {answer: 42};
+
+	render() {
+		return (
+			<h2>Hello Components {this.state.answer}</h2>
+		);
+	}
+}
 
 export default App;
